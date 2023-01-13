74009bffba6a4acae3dc1953fc42fd647f89fb01
IAGO SOARES <iago.jt21@gmail.com>
2023-01-13 15:53:20 -0300

    ok

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDiQWU3Yo_uvcw9lfajZHTnQlQQE29jmmI",
+    authDomain: "quizrealtime-278ed.firebaseapp.com",
+    projectId: "quizrealtime-278ed",
+    storageBucket: "quizrealtime-278ed.appspot.com",
+    messagingSenderId: "700921083925",
+    appId: "1:700921083925:web:f185de01d35f67c521361c"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
